7249ea00ff48da97d6f32bc8b01c5add088021b3
ascorbic <ascorbic@gmail.com>
2018-05-24 07:22:10 +0100

    Tidying

--- template/preact.config.js
+++ template/preact.config.js
@@ -1,13 +1,9 @@
-/**
- * This is mostly lifted from preact-cli-plugin-typescript,
- * but with ts-loader instead of ATL, as I couldn't get it to work.
- */
 import { resolve } from "path";
 
 export default function(config, env, helpers) {
     // Switch css-loader for typings-for-css-modules-loader, which is a wrapper
     // that automatically generates .d.ts files for loaded CSS
-    helpers.getLoadersByName(config, "css-loader").map(({ loader }) => {
+    helpers.getLoadersByName(config, "css-loader").forEach(({ loader }) => {
         loader.loader = "typings-for-css-modules-loader";
         loader.options = Object.assign(loader.options, {
             camelCase: true,
@@ -18,17 +14,12 @@ export default function(config, env, helpers) {
         });
     });
 
-    // This is lifted from preact-cli-plugin-typescript,
-    // but with ts-loader instead of ATL, as I couldn't get it to work.
-
     config.module.loaders.push({
         test: /\.[tj]sx?$/,
         loader: "ts-loader"
     });
 
-    // Currently, preact-cli only looks for `src/index.js` - this will look
-    // for any file in `src` named `index` that has an extension that's
-    // resolved by webpack. This can be overridden by the user.
+    // Use any `index` file, not just index.js
     config.resolve.alias["preact-cli-entrypoint"] = resolve(
         process.cwd(),
         "src",
