e13b569c0c6147475670dc74ef671934e225b4f1
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-25 19:17:19 +0900

    use @typescript-eslint/recommended-requiring-type-checking and react-hooks plugins

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,21 +2,27 @@ module.exports = {
     env: {
         browser: true
     },
+    plugins: ["@typescript-eslint", "react-hooks"],
     extends: [
-        "plugin:react/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
         "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+        "prettier/react"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
             jsx: true
         },
-        ecmaVersion:  2018,
-        sourceType: "module",
+        project: "./tsconfig.json",
     },
     rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
         "react/no-unknown-property": ["error", { ignore: ["class"] }],
     },
     settings: {
