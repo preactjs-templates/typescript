b9a90b03bb28941cde858c9508d3468920f20556
Hiroaki KARASAWA <karszawa@users.noreply.github.com>
2020-04-25 10:29:53 +0900

    fix: do not convert style import

--- template/jest.config.js
+++ template/jest.config.js
@@ -23,7 +23,6 @@ module.exports = {
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.js",
         "\\.(css|less|scss)$": "identity-obj-proxy",
-        "^./style$": "identity-obj-proxy",
         "^preact$": "<rootDir>/node_modules/preact/dist/preact.min.js",
         "^react$": "preact-compat",
         "^react-dom$": "preact-compat",
