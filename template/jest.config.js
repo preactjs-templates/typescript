1fc95f8c934b897755045b71c9b7e2b3e3e17fb3
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2020-05-06 21:21:17 +0200

    Remove create-react-class module mapper (create-react-class does not exist in Preact X compat)

--- template/jest.config.js
+++ template/jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
     testURL: "http://localhost:8080",
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.js",
-        "^create-react-class$": "preact-compat/lib/create-react-class",
         "^react-addons-css-transition-group$": "preact-css-transition-group"
     }
 }
