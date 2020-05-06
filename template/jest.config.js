e23db863dc9f20f0e20661d0bec4975d18ce033d
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2020-05-06 21:15:40 +0200

    Use jest-preset-preact as much as possible instead of manually configuring jest

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,26 +1,10 @@
 module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    verbose: true,
+    preset: "jest-preset-preact",
     setupFiles: [
         "<rootDir>/src/tests/__mocks__/setupTests.js",
         "<rootDir>/src/tests/__mocks__/browserMocks.js"
     ],
     testURL: "http://localhost:8080",
-    moduleFileExtensions: [
-        "js",
-        "jsx",
-        "ts",
-        "tsx"
-    ],
-    moduleDirectories: [
-        "node_modules"
-    ],
-    testMatch: [
-        "**/__tests__/**/*.[jt]s?(x)",
-        "**/?(*.)(spec|test).[jt]s?(x)"
-    ],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.js",
         "\\.(css|less|scss)$": "identity-obj-proxy",
