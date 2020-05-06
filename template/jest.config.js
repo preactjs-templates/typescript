c83e5605eb2b36587cdbb0bbcbea1bebaa15fa80
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2020-05-06 21:20:43 +0200

    Remove moduleNameMapper entries already covered by jest-preset-preact

--- template/jest.config.js
+++ template/jest.config.js
@@ -7,11 +7,6 @@ module.exports = {
     testURL: "http://localhost:8080",
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.js",
-        "\\.(css|less|scss)$": "identity-obj-proxy",
-        "^./style$": "identity-obj-proxy",
-        "^preact$": "<rootDir>/node_modules/preact/dist/preact.min.js",
-        "^react$": "preact-compat",
-        "^react-dom$": "preact-compat",
         "^create-react-class$": "preact-compat/lib/create-react-class",
         "^react-addons-css-transition-group$": "preact-css-transition-group"
     }
