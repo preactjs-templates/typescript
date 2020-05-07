93d911525f1a693b911ea327d23d428c224dfa7f
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2020-05-07 17:20:56 +0200

    Fix file mocks setup path (See: https://github.com/preactjs-templates/default/pull/25)

--- template/jest.config.js
+++ template/jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
     ],
     testURL: "http://localhost:8080",
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMocks.js",
     }
 }
