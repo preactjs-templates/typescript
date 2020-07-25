f701f25d8632b95e812af9de1a364311812e7847
frantss <frantss11@gmail.com>
2020-07-24 22:47:18 -0300

    fix: add ts-jest and change jes.config.js

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
     preset: "jest-preset-preact",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
     setupFiles: [
         "<rootDir>/src/tests/__mocks__/setupTests.js",
         "<rootDir>/src/tests/__mocks__/browserMocks.js"
     ],
     testURL: "http://localhost:8080",
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMocks.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+            "<rootDir>/src/tests/__mocks__/fileMocks.js"
     }
-}
+};
