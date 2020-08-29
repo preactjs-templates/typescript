f9de4451c8c5a855e2136b1544a742997818bd61
Ryan Christian <ryanchristian4427@gmail.com>
2020-08-29 16:40:38 +0100

    refactor: Removing preact/debug import as unnecessary

--- template/src/components/app.tsx
+++ template/src/components/app.tsx
@@ -6,12 +6,6 @@ import Profile from "../routes/profile";
 import NotFoundPage from "../routes/notfound";
 import Header from "./header";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-if ((module as any).hot) {
-    // tslint:disable-next-line:no-var-requires
-    require("preact/debug");
-}
-
 const App: FunctionalComponent = () => {
     let currentUrl: string;
     const handleRoute = (e: RouterOnChangeArgs) => {
