f6a190739ff9a480c128b3469a7b387da17bf8dc
Ryan Christian <rchristian@ryanchristian.dev>
2023-01-10 09:42:32 -0600

    chore: Add default empty `preact.config.js` file

---
+++ template/preact.config.js
@@ -0,0 +1,12 @@
+/**
+ * Function that mutates the original webpack config.
+ * Supports asynchronous changes when a promise is returned (or it's an async function).
+ * You can find more info here: https://github.com/preactjs/preact-cli#webpack
+ *
+ * @param {import('preact-cli').Config} config - original webpack config
+ * @param {import('preact-cli').Env} env - current environment and options pass to the CLI
+ * @param {import('preact-cli').Helpers} helpers - object with useful helpers for working with the webpack config
+ */
+export default (config, env, helpers) => {
+	/** you can change the config here **/
+};
